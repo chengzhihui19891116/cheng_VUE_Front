b9d36927e0bfb137e789f11f679cccf8e076eab1
czhcheng <czhcheng@cn.ibm.com>
2019-11-14 11:10:41 +0800

    update by czhcheng

--- src/components/root/root.vue
+++ src/components/root/root.vue
@@ -1,16 +1,57 @@
 <template>
  <div>
-    <div style="float:left"><h3>Male</h3></div></br></br>
-    <div style="float:left">
-      <div  v-for="(item,index) in showMalePetsArr" :key="index">{{item.name}}</div>
-    </div>
-    </br>
-    <div style="margin-top:100px">
-      <div style="float:left"><h3>Female</h3></div><br></br>
-      <div style="float:left">
-          <div  v-for="(item,index) in showFemalePetsArr" :key="index">{{item.name}}</div>
+        <div style="float:left"><h3>Male</h3></div></br></br>
+        <div style="float:left">
+          <div  v-for="(item,index) in showMalePetsArr" :key="index">{{item.name}}</div>
+        </div>
+        </br>
+
+        <div style="margin-top:100px">
+          <div style="float:left"><h3>Female</h3></div><br></br>
+            <div style="float:left">
+                <div  v-for="(item,index) in showFemalePetsArr" :key="index">{{item.name}}</div>
+            </div>
+        </div>
+
+        <div style="margin-top:150px;width:200px">
+              <el-dropdown  split-button type="primary" @command="handleCommand">
+                Weather for china
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item command="大连">大连</el-dropdown-item>
+                  <el-dropdown-item command="朝阳">朝阳</el-dropdown-item>
+                  <el-dropdown-item command="凌源">凌源</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+        </div>
+  
+    <div style="width:250px">
+      <div>
+        <div style="float:left;" v-if="this.cityChoose == '大连'">
+            <div style="float:left;margin-left:10px;">City:{{cityMsg.city}}</div></br>
+            <div style="float:left;margin-left:10px;">Update time:{{my_todayWeather.date}}</div></br>
+            <div style="float:left;margin-left:10px;">Weather:{{my_todayWeather.type}}</div></br>
+            <div style="float:left;margin-left:10px;">Temperature:{{my_todayWeather.high}} {{my_todayWeather.low}}</div></br>
+            <div style="float:left;margin-left:10px;">Wind:{{my_todayWeather.fengxiang}}</div></br>
+        </div>
       </div>
-</div>
+
+        <div style="float:left;" v-if="this.cityChoose == '朝阳'">
+            <div style="float:left;margin-left:10px;">City:{{cityMsg.city}}</div></br>
+            <div style="float:left;margin-left:10px;">Update time:{{my_todayWeather.date}}</div></br>
+            <div style="float:left;margin-left:10px;">Weather:{{my_todayWeather.type}}</div></br>
+            <div style="float:left;margin-left:10px;">Temperature:{{my_todayWeather.high}} {{my_todayWeather.low}}</div></br>
+            <div style="float:left;margin-left:10px;">Wind:{{my_todayWeather.fengxiang}}</div></br>
+        </div>
+        <div style="float:left;" v-if="this.cityChoose == '凌源'">
+            <div style="float:left;margin-left:10px;">City:{{cityMsg.city}}</div></br>
+            <div style="float:left;margin-left:10px;">Update time:{{my_todayWeather.date}}</div></br>
+            <div style="float:left;margin-left:10px;">Weather:{{my_todayWeather.type}}</div></br>
+            <div style="float:left;margin-left:10px;">Temperature:{{my_todayWeather.high}} {{my_todayWeather.low}}</div></br>
+            <div style="float:left;margin-left:10px;">Wind:{{my_todayWeather.fengxiang}}</div></br>
+        </div>
+    </div>
+
+
 
 </div>
 </template>
@@ -29,13 +70,34 @@ export default {
    data(){
      return{
         showMalePetsArr:[],
-        showFemalePetsArr:[]
+        showFemalePetsArr:[],
+        cityChoose:'',
+        my_todayWeather:'',
+        cityMsg:''
      } 
    },
    computed: {
-
+      
     },
    methods:{
+     // the method for Weather
+    handleCommand(command) {
+      this.cityChoose = command
+      this.$axios
+          .get(
+           'http://localhost:5050/getWeather/' + command + '/' +'?timeid=' + (new Date()).valueOf(), 
+          )
+           .then(res => {
+              var futherWeather = res.data.data
+              var futher5Days = futherWeather.forecast
+              var todayWeather = futher5Days[0]
+              this.my_todayWeather = todayWeather
+              this.cityMsg= futherWeather
+            }).catch(function (error) {
+              console.log(error);
+          })
+    },
+    // the method for order cat
     fetchData(){
         this.$axios
           .get(
@@ -78,7 +140,7 @@ export default {
             this.showMalePetsArr = maleCatsArr
           }).catch(function (error) {
               console.log(error);
-          });;
+          });
       },
    }
   
--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-
-
 import axios from 'axios'
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
